642d727d43ed28f7ca491cf7548875d5c1879ddd
acxz <17132214+acxz@users.noreply.github.com>
2022-03-10 17:33:47 -0500

    use setup.py instead of setup.cfg for package edit mode

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="blinding-ray",
+    version="0.0.1",
+    author="acxz",
+    description="blinding-ray description",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/acxz/blinding-ray",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+)
