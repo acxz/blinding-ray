0df3b3ec9f7c2a3c2d57c2e803bcb2607689162b
acxz <17132214+acxz@users.noreply.github.com>
2022-03-21 21:39:52 -0400

    add AttackerTrainer to test

--- scripts/test_open_spiel_env.py
+++ scripts/test_open_spiel_env.py
@@ -1,30 +1,24 @@
 import pyspiel
-
 from ray.rllib.agents.ppo import PPOTrainer
 from ray.rllib.env.wrappers.open_spiel import OpenSpielEnv
 from ray.tune import register_env
 
+from blinding_ray.agents.attacker import AttackerTrainer
 from blinding_ray.agents.random import RandomTrainer
 
 # RBC OpenSpiel env
 register_env("open_spiel_env_rbc", lambda _: OpenSpielEnv(
     pyspiel.load_game("rbc")))
-    #pyspiel.load_game("connect_four")))
 
 # Configure the algorithm
 config = {
     "env": "open_spiel_env_rbc",
-    "model": {
-        "fcnet_hiddens": [512, 512],
-        "fcnet_activation": "relu",
-    },
     "num_workers": 1,
-    "framework": "torch",
 }
 
 # Create our RLlib trainer
-#trainer = PPOTrainer(config=config)
 trainer = RandomTrainer(config=config)
+# trainer = AttackerTrainer(config=config)
 
 # Run training iterations
 iterations = 1
